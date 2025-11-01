2bf97f17052903eb57d8621153afdabf0edddc88
Jack Greer <jvpng1@gmail.com>
2025-11-01 09:42:57 -0600

    rendering board a little better

--- game.cpp
+++ game.cpp
@@ -4,17 +4,58 @@
 Board::Board(void){
     length = 8;
     width = 8;
+
+    for(int row = 0; row < length; row++){
+        for(int col = 0; col < width; col++){
+            tiles[row][col] = Tile(((row + col) % 2) ? Black : White);
+        }
+    }
 }
 
 void Board::Render(void){
 
-    for(int row = length; row > 0; row--){
-	std::cout << (char)('0' + row);
-        for(int col = 0; col < width; col++){
-            std::cout << (((row + col) % 2) ? BSQUARE : WSQUARE);
+//    for(int row = length; row > 0; row--){
+//        std::cout << (char)('0' + row);
+//        for(int col = 0; col < width; col++){
+//            tiles[row-1][col].Render();
+//        }
+//        std::cout << std::endl;
+//    }
+//
+//    std::cout << " ABCDEFGH" << std::endl;
+
+    // Goofy ahh methodology: just print massive board
+    for(int row = 0; row < 8; row++){
+        RenderRow(row);
+    }
+}
+
+void Board::RenderRow(int row){
+
+    Color currentColor = tiles[row][0].color;
+
+    for(int tile_y = 0; tile_y < 3; tile_y++){
+        for(int col = 0; col < 8; col++){
+            // Switch color tiles here because this is where we actually switch squares
+            currentColor = (currentColor == White) ? Black : White;
+            for(int tile_x = 0; tile_x < 3; tile_x++){
+                std::cout << ((tiles[row][col].color == Black) ? BSQUARE : WSQUARE);
+            }
         }
         std::cout << std::endl;
     }
 
-    std::cout << " ABCDEFGH" << std::endl;
+}
+
+Tile::Tile(void){
+    color = White;
+}
+
+Tile::Tile(Color inColor){
+    color = inColor;
+}
+
+void Tile::Render(void){
+    
+    std::cout << ((color == Black) ? BSQUARE : WSQUARE);
 }
--- game.h
+++ game.h
@@ -6,6 +6,34 @@
 #define WSQUARE "\u25A1"
 #define BSQUARE "\u25A0"
 
+typedef enum {White, Black} Color;
+
+// Piece class attributes...
+// needs a color, maybe list of moves?
+// also make sure to do that funny trick where you have to define the function in the child class
+class Piece
+{
+  public:
+    Piece(void);
+    Piece(Color inColor);
+    Color color;
+};
+
+class Pawn : public Piece
+{
+  public
+};
+
+class Tile
+{
+  public:
+    Color color;
+    Tile(void);
+    Tile(Color inColor);
+    void Render(void);
+
+};
+
 class Board
 {
   public:
@@ -13,5 +41,8 @@ class Board
     int width;
     Board(void);
     void Render(void);
+    void RenderRow(int row);
+    Tile tiles[8][8];
 };
+
 #endif // GAME_H_
